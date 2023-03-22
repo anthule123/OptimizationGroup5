054a41d91ee8f977e91e93745d4e083238cd4249
Hungk66tntoan <92563490+Hungk66tntoan@users.noreply.github.com>
2023-03-22 20:29:23 +0700

    Update Nhom05_BaiTap04_AnHung.py

--- Nhom05_BaiTap04_AnHung.py
+++ Nhom05_BaiTap04_AnHung.py
@@ -9,51 +9,41 @@ class ColorProblem:
         self.N = N
         self.k = k
         self.model = gp.Model("ColorProblem")
-        self.x = [[self.model.addVar(vtype=GRB.BINARY,
-                                      name = f'x_{i}_{j}')
-                                        for i in range(0, self.N + 1)]
-                                          for j in range(0, self.k)]
-        for color in range(0, self.k):
-            self.model.addConstr(self.x[color][0]==0)
-        self.listOfThreeSum = []
-        self.setListOfThreeSum()
-        return None
+        self.colorSet = [i for i in range(k)]
+        self.numSet = [i + 1 for i in range(N)]
+        self.x = [[self.model.addVar(vtype=GRB.BINARY, name = f'x_{num - 1}_{color}')
+              for color in self.colorSet] for num in self.numSet] # biến x_i_j chỉ báo việc màu j được gán cho số i hay không.
     def setOneColorConstraints(self):
-        for i in range(1, self.N + 1):
-            return self.model.addConstr(sum(self.x[j][i] 
-                                            for j in range(self.k)) == 1)
-    def setListOfThreeSum(self):
+        # mỗi số được tô đúng một màu
+        for num in self.numSet:
+            self.model.addConstr(sum(self.x[num - 1][color] for color in self.colorSet) == 1)
+    @property
+    def listOfThreeSum(self):
+        # tạo list chứa các bộ số a < b < c sao cho a + b = c
+        listOfListThreeSum = []
         halfOfN = (self.N + 1) // 2
-        for numA in range(1, halfOfN):
-            for numB in range(numA + 1, self.N - numA + 1):
-                numC = numA + numB
-                self.listOfThreeSum.append([numA, numB, numC])
+        rangeOfNumA = [num1 for num1 in range(1, halfOfN)]
+        for numA in rangeOfNumA:
+            rangeOfNumB = [num2 for num2 in range(numA + 1, self.N - numA + 1)]
+            for numB in rangeOfNumB:
+                listOfListThreeSum.append([numA, numB, numA + numB])
+        return listOfListThreeSum
+    
     def setSumColorConstraints(self):
+        #Trong 3 số a < b < c mà a + b = c thì mỗi màu dùng không quá 2 lần
         for threeNumber in self.listOfThreeSum:
-            for l in range(0, self.k):
-                self.model.addConstr(sum(self.x[l][number] 
-                                         for number in threeNumber)
-                                          <= 2)
-        self.model.addConstr(self.x[0][1] + self.x[0][2] + self.x[0][3] <= 2)
-    def setNNumbersConstraints(self):
-        #tổng tất cả các màu phải bằng N
-        self.model.addConstr(sum(self.x[l][i] 
-                                        for l in range(self.k)
-                                            for i in range(1, self.N + 1)) 
-                                            == self.N)
-
+            for color in self.colorSet:
+                self.model.addConstr(sum(self.x[number - 1][color] for number in threeNumber) <= 2)
+    
     def setObjective(self):
-        self.model.setObjective(sum(self.x[0][i] for i in range(1, self.N + 1)),
-                                sense = GRB.MINIMIZE)
+        self.model.setObjective(0, sense = GRB.MAXIMIZE)
+        
     def solve(self):
         self.setOneColorConstraints()
         self.setSumColorConstraints()
-        self.setNNumbersConstraints()
         self.setObjective()
         self.model.optimize()
-        
-
-modelColor = ColorProblem(26, 2)
+    
+modelColor = ColorProblem(11, 3)
 print(modelColor.listOfThreeSum)
 modelColor.solve()
-        
